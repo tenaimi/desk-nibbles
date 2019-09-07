b018e0d2baed006cfab835ddc6534e1dc97af87c
Tenaimi Okurude <tjokurude@gmail.com>
2019-09-06 23:03:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 app.use(logger("dev"));
 app.get("/snackers", (req, res) => {
     axios.get("https://s3.amazonaws.com/misc-file-snack/MOCK_SNACKER_DATA.json").then(res1 => {
